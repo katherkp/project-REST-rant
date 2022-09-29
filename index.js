cc655625be45cca1190994ab22a90cfcbe3bf6fe
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-09-29 13:28:05 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express', 'dotenv').config()
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
