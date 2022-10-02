22fae7bd72387d5cf0b90b9b30adb814a02e20a8
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-10-02 09:46:44 -0700

    jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,31 @@ function new_form () {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                <form method ="POST" action="/places">
+                    <div>
+                        <label htmlFor="name">Place Name</label>
+                        <input id ="name" name="name" required />
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id="pic" name="pic" required />
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input id="city" name="city" required />
+                    </div>
+                    <div>
+                        <label htmlFor="state">State</label>
+                        <input id="state" name="state" required />
+                    </div>
+                    <div>
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input id="cuisines" name="cuisines" required />
+                    </div>
+                    <div>
+                        <input type="submit" value="Add Place" />
+                    </div>
+                </form>
             </main>
         </Def>
     )
