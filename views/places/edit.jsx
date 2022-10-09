e13863f171fcbc97e45c56c1086e94a69cabe58a
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-10-09 15:44:30 -0700

    populating data for star ratings

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,41 +1,63 @@
 const React = require('react')
 const Def = require('../default')
 
-function edit_form (data) {
+function edit_form(data) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method ="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                     <div className="row">
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="name">Place Name</label>
                             <input
                                 className="form-control"
                                 id="name"
+                                name="name"
                                 value={data.place.name}
-                            required />
+                                required />
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input
+                                className="form-control"
+                                id="pic"
+                                name="pic"
+                                value={data.place.pic} />
                         </div>
                     </div>
-                    <div className ="form-group col-sm-6">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input type="url" id="pic" name="pic" required />
+                    <div className="row">
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
+                            <label htmlFor="city">City</label>
+                            <input
+                                className="form-control"
+                                id="city"
+                                name="city"
+                                value={data.place.city} />
+                        </div>
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
+                            <label htmlFor="state">State</label>
+                            <input
+                                className="form-control"
+                                id="state"
+                                name="state"
+                                value={data.place.state} />
+                        </div>
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
+                            <label htmlFor="founded">Founded</label>
+                            <input 
+                                className="form-control" 
+                                id="founded" 
+                                name="founded" 
+                                value={data.place.founded} 
+                                />
+                        </div>
                     </div>
-                    <div>
-                        <label htmlFor="city">City</label>
-                        <input id="city" name="city" required />
-                    </div>
-                    <div>
-                        <label htmlFor="state">State</label>
-                        <input id="state" name="state" required />
-                    </div>
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
-                        <input id="cuisines" name="cuisines" required />
+                        <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
                     </div>
-                    <div>
-                        <input type="submit" value="Add Place" />
-                    </div>
+                    <input className="btn btn-primary" type="submit" value="Update Place" />
                 </form>
             </main>
         </Def>
