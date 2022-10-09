4371a6756b6fd0ef9c387314c8fd7af5907de8e2
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-10-09 14:41:41 -0700

    commentary for rants and raves

--- controllers/places.js
+++ controllers/places.js
@@ -32,16 +32,16 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-    console.log(place.comments)
-    res.render('places/show', { place })
+  db.Place.findById(req.params.id) 
+  .populate("comments")
+  .then((place) => {
+    console.log(place.comments);
+    res.render("places/show", { place });
   })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
+  .catch((err) => {
+    console.log("err", err);
+    res.render("error404");
+  });
 })
 
 router.put('/:id', (req, res) => {
@@ -58,6 +58,21 @@ router.get('/:id/edit', (req, res) => {
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
+})
+
+router.post('/:id/comment', (req,res) =>{
+  console.log(req.body)
+  db.Place.findById(req/params.id)
+  .then(comment => {
+      place.comments.push(comment.id)
+      place.save()
+      .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+        })
+     })
+  .catch(err => {
+    res.render('error404')
+  })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
